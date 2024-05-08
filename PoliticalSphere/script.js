501d60ff80e5d340cc1c69b98c643873ef12b204
Venalitas <miniklip99@gmail.com>
2024-05-08 14:52:46 +0300

    Update script.js

--- PoliticalSphere/script.js
+++ PoliticalSphere/script.js
@@ -7,3 +7,27 @@ function ButtonHideJPG() {
     image.style.display = "none";
   }
 }
+
+const list = document.getElementById("container");
+const sortButton = document.getElementById("container-sort-button");
+
+sortButton.addEventListener("click", SortingList() {
+  const listItems = list.querySelectorAll(".item-container");
+  const itemsArray = Array.from(listItems);
+
+  
+  function compareNames (a, b)
+  {
+    const nameA = a.querySelector(".item-text-name").textContent;
+    const nameB = b.querySelector(".item-text-name").textContent;
+    return nameA.localeCompare(nameB); // Locale-aware comparison
+  }
+  
+  itemsArray.sort(compareNames);
+
+  // Clear the list content and re-populate with sorted elements (same as before)
+  list.innerHTML = "";
+  itemsArray.forEach(function(item-container) {
+    list.appendChild(item-container);
+  });
+}
