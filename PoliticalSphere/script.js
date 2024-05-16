d8b80d85235223c45981810029bffe07c55866a7
Venalitas <miniklip99@gmail.com>
2024-05-16 14:35:08 +0300

    Update script.js

--- PoliticalSphere/script.js
+++ PoliticalSphere/script.js
@@ -34,6 +34,6 @@ function SortingListPartyJV() {
   const listItems = list.querySelectorAll(".ItemContainer");
   const itemsArray = Array.from(listItems);
 
-  console.log("listItems.lenght: ", listItems.lenght);
+  console.log("listItems.lenght: ", listArray.lenght);
 
 }
